819a4cd149295b680a6c313341a7c09b98c541b9
Nilang Thakker <64350867+nilang003@users.noreply.github.com>
2022-11-10 02:04:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import telebot
 bot = telebot.TeleBot("5797737730:AAGLdTGW6wiLIlM-EvYGOynNg6cu6-Tj_-A")
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "Howdy, Bahut hui bakchodi?")
 
 
 
