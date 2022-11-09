ad04804b2668de8f711d36280e9481e84e802899
Nilang Thakker <64350867+nilang003@users.noreply.github.com>
2022-11-10 01:39:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 import requests
-from app import cricFuntionality
+
 bot = telebot.TeleBot("5797737730:AAGLdTGW6wiLIlM-EvYGOynNg6cu6-Tj_-A")
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
